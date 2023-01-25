2f5044fa8398e449ba01d6b15d4a2fb935ff0efb
Yordanka Toteva <yordanka.toteva@gmail.com>
2023-01-25 10:17:32 +0200

    Code update to extract a file from a zip file.

---
+++ wex-onboarding/Operations/ZipFileExtract.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace wex_onboarding.Operations
+{
+    public static class ZipFileExtract
+    {
+        public static string ExtractAndCopyFileFromZIP(string srcFile, string destFolder)
+        {
+            string pathToExtractedFile = String.Empty;
+            using (ZipArchive zip = ZipFile.Open(srcFile, ZipArchiveMode.Read))
+            {
+                foreach (ZipArchiveEntry entry in zip.Entries)
+                {
+                    string fileName = entry.Name;
+                    pathToExtractedFile = Path.Combine(destFolder, fileName);
+                    var directoryName = Path.GetDirectoryName(pathToExtractedFile);
+
+                    if (directoryName != null) Directory.CreateDirectory(directoryName);
+
+                    if (!fileName.Equals("[Content_Types].xml"))
+                    {
+                        entry.ExtractToFile(pathToExtractedFile);
+                        break;
+                    }
+                }
+            }
+
+            return pathToExtractedFile;
+        }
+
+        
+        public static void DeleteFileAndDirectory(string pathToExtractedFile)
+        {
+            string directoryName = Path.GetDirectoryName(pathToExtractedFile);
+
+            if (Directory.Exists(directoryName))
+            {
+                Directory.Delete(directoryName, true);
+            }
+        }
+    }
+}
--- wex-onboarding/UnitTests.cs
+++ wex-onboarding/UnitTests.cs
@@ -4,20 +4,30 @@ using wex_onboarding.XMLToObject;
 
 namespace wex_onboarding
 {
+    [TestFixture]
     public class UnitTests
     {
-        private readonly string filePath = @"Files\InputFile.xml";
+        private readonly string srcFile = @"Files\SNC_AOD_01102023_061442.xml.zip";
+        private readonly string destFolder = "OutputFiles";
+        private string pathToExtractedFile = null;
 
         [Test]
         public void VerifyTheTotalPaidAmount()
         {
             decimal expectedPaidAmountTotal = 3.4900M;
 
-            PaymentFile paymentFile = XMLDeserialization.DeserializeToObject<PaymentFile>(filePath);
+            pathToExtractedFile = ZipFileExtract.ExtractAndCopyFileFromZIP(srcFile, destFolder);
+            PaymentFile paymentFile = XMLDeserialization.DeserializeToObject<PaymentFile>(pathToExtractedFile);
             decimal actualPaidAmountTotal = Methods.ReturnTotalPaidAmount(paymentFile.PaymentDetail.ReimbursementEOB.PlanBalanceTable);
 
             Assert.AreEqual(expectedPaidAmountTotal, actualPaidAmountTotal, $"The expected total paid amount ({expectedPaidAmountTotal}) " +
                 $"is different from the actual one ({actualPaidAmountTotal}).");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ZipFileExtract.DeleteFileAndDirectory(pathToExtractedFile);
         }
     }
 }
